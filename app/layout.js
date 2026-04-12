285df2d7a0ad1b9674b0a685557e2e372f45b42f
a18502656182-arch <a18502656182@gmail.com>
2026-04-12 10:45:25 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import PenguinWrapper from "./components/PenguinWrapper";
+import BuyFloatBtn from "./components/BuyFloatBtn";
 import WelcomeModal from "./components/WelcomeModal";
 
 export const metadata = {
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
       <body style={{ margin: 0 }}>
         {children}
         <PenguinWrapper />
+        <BuyFloatBtn />
         <WelcomeModal />
       </body>
     </html>
